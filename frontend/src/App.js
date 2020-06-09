e82b66c4bde68986c3cb2b8b968ae00d2b7ca477
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-09 14:10:33 -0300

    Cadastrando Projetos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,9 +16,17 @@ function App() {
     })
   }, []);
 
-  function handleAddProject() {
+  async function handleAddProject() {
     // projects.push(`Novo projeto ${Date.now()}`);
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    const response = await api.post('/projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Janderson Vilas Boas'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
