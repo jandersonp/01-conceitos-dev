5d2f0338f6cd39cdad3ae8347e7eac7ab7e25f28
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-09 10:43:32 -0300

    Propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header title="cabecalho 1"/>
-      <Header title="cabecalho 2"/>
+      <Header title="cabecalho 1">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="cabecalho 2">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function Header(props) {
+export default function Header({ title, children }) {
   return (
     <header>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
+
+      {children}
+
     </header>
   )
 }
