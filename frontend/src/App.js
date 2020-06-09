71c538914e3c14e64b48ca50c9b61f12d31bfb26
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-09 10:35:08 -0300

    Componentização

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header title="cabecalho 1"/>
+      <Header title="cabecalho 2"/>
+    </>
+  );
+}
+
+export default App;
