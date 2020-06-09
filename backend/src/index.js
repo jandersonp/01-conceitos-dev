dbedb7c0b89a4693857c46ae0688f099aec191ed
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-09 09:37:49 -0300

    tipos de parâmetros, Aplicação funcional

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,72 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
-})
+app.use(express.json());
 
-app.listen(3333);
+const projects = [];
+
+//Route: List projects
+app.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const results = title ? projects.filter(project => project.title.includes(title)) : projects;
+
+  return response.json(results);
+ 
+});
+
+//Route: Create projects
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner};
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+
+//Route: Update projects
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({error: 'Project not found'})
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+//Route: Delete projects
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({error: 'Project not found'})
+  }
+
+  projects.splice(projectIndex, 1);
+
+
+  return response.status(204).send();
+});
+
+app.listen(3333, () => {
+  console.log('✔ Backend started ')
+});
